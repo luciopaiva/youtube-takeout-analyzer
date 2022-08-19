948246e3d824889622c0b31f63953418a69d0b58
Lucio Paiva <luciopaiva@gmail.com>
2022-08-19 19:11:35 +0100

    Expand top lists via cutoff

--- watch-history-top-channels.js
+++ watch-history-top-channels.js
@@ -1,8 +1,7 @@
 
 const Digest = require("./watch-history-digest");
 
-const TOP_COUNT = 20;
-const VIDEO_NAME_PADDING = 100;
+const CUTOFF = 5;
 
 ((async function () {
     const digest = await Digest.load();
@@ -11,10 +10,10 @@ const VIDEO_NAME_PADDING = 100;
     const channels = [...digest.channelByUrl.values()];
     channels.sort((a, b) => b.views - a.views);
 
-    console.info(`\nTop ${TOP_COUNT} most watched channels:`);
-    for (let i = 0; i < TOP_COUNT; i++) {
-        const channel = channels[i];
-        const name = channel.name.padEnd(VIDEO_NAME_PADDING, " ");
-        console.info(`- ${name} ${channel.views}`);
+    for (const channel of channels) {
+        if (channel.views < CUTOFF) {
+            break;
+        }
+        console.info(`${channel.name},${channel.views}`);
     }
 }))();
--- watch-history-top-videos.js
+++ watch-history-top-videos.js
@@ -1,8 +1,7 @@
 
 const Digest = require("./watch-history-digest");
 
-const TOP_COUNT = 20;
-const VIDEO_NAME_PADDING = 100;
+const CUTOFF = 2;
 
 ((async function () {
     const digest = await Digest.load();
@@ -11,10 +10,10 @@ const VIDEO_NAME_PADDING = 100;
     const videos = [...digest.videoByUrl.values()];
     videos.sort((a, b) => b.views - a.views);
 
-    console.info(`\nTop ${TOP_COUNT} most watched videos:`);
-    for (let i = 0; i < TOP_COUNT; i++) {
-        const video = videos[i];
-        const name = video.name.padEnd(VIDEO_NAME_PADDING, " ");
-        console.info(`- ${name} ${video.views}`);
+    for (const video of videos) {
+        if (video.views < CUTOFF) {
+            break;
+        }
+        console.info(`${video.name},${video.views}`);
     }
 }))();
